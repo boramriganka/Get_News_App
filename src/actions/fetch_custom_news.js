1da65311b27be4775070f00cb0546bf564db2809
boramriganka <xorai.mriganka@gmail.com>
2020-12-01 19:19:05 +0530

    first commit

---
+++ src/actions/fetch_custom_news.js
@@ -0,0 +1,22 @@
+const api = process.env.REACT_APP_KEY_NEWS;
+console.log(api);
+
+export function fetchCustomNews(source, relevance) {
+	//return the actual action to do
+
+	return function (dispatch) {
+		const url = `https://newsapi.org/v1/articles?source=${source}&sortBy=${relevance}&apiKey=${API}`;
+		fetch(url)
+			.then((res) => {
+				console.log(url);
+				return res.json();
+			})
+			.then((res) => {
+				// console.log(res)
+				dispatch({ type: 'FETCH_CUSTOM_NEWS', payload: res.articles });
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+}
