4ec8a99955cd0956db8d7e9c1abd3e0ff1511ff3
Mriganka Bora <42547729+boramriganka@users.noreply.github.com>
2026-05-26 14:59:01 +0530

    Resolve merge conflicts: Fix double BrowserRouter wrapping and update imports

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//router v4
-import { BrowserRouter } from 'react-router-dom'
-
 ReactDOM.render(
-<BrowserRouter>
- <App />
-</BrowserRouter>, document.getElementById('root'));
+  <App />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
